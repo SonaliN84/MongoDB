2defd2597abb4df785249689b05b73f0b5f8bcf8
SonaliN84 <sonali.nilekar@gmail.com>
2023-06-20 00:16:41 +0530

    MongoDB connected

---
+++ app.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const env=require('dotenv');
+env.config();
+
+const errorController = require('./controllers/error');
+
+const mongoConnect =require('./util/database').mongoConnect
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+ next();
+});
+
+app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
+
+// app.use(errorController.get404);
+
+mongoConnect(()=>{
+  
+  app.listen(3000);
+});
+
---
+++ util/database.js
@@ -0,0 +1,30 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    process.env.MONGODB
+  )
+    .then((client) => {
+      console.log("connected");
+      _db=client.db()
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb=()=>{
+  if(_db){
+    return _db;
+  }
+  throw 'No database found'
+}
+module.exports ={ 
+  mongoConnect,
+  getDb
+}

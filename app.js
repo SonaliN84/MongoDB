610ae5943be0f9c258daf6e762013c8e1b05e18a
SonaliN84 <sonali.nilekar@gmail.com>
2023-06-20 18:59:03 +0530

    get,add,edit,delete product with mongoose

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
-const path = require('path');
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const env=require('dotenv');
+const path = require("path");
+const env = require("dotenv");
 env.config();
-const User=require('./models/user')
-const errorController = require('./controllers/error');
+const mongoose = require("mongoose");
 
-const mongoConnect =require('./util/database').mongoConnect
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const User = require("./models/user");
+const errorController = require("./controllers/error");
+
+
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
- User.findById('649124b882f855f7c699a9ca')
- .then(user=>{
-  req.user=new User(user.name,user.email,user.cart,user._id);
-  next();
- })
- .catch(err=>{
-  console.log(err)
- })
-});
+// app.use((req, res, next) => {
+//   User.findById("649124b882f855f7c699a9ca")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 // app.use(errorController.get404);
 
-mongoConnect(()=>{
-  
-  app.listen(3000);
-});
-
+mongoose
+  .connect('mongodb+srv://sonalinilekar:TKvjzWWw6o1ByuHZ@cluster0.3axas1n.mongodb.net/shop?retryWrites=true&w=majority')
+  .then((result) => {
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>connected")
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });

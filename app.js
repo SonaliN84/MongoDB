328a4300310675f3c6f0e914083d79a05c4355ce
SonaliN84 <sonali.nilekar@gmail.com>
2023-06-20 13:31:49 +0530

    Established realation between user ans product collection

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
  User.findById('649124b882f855f7c699a9ca')
  .then(user=>{
-  req.user=user;
+  req.user=new User(user.name,user.email,user.cart,user._id);
   next();
  })
  .catch(err=>{
